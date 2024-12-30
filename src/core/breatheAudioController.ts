e2142cfe2d3ee1764b5973c6c55936f5a7e790a7
Denisoed <denisoed@gmal.com>
2024-12-30 18:49:28 +0600

    Added volume for breathe audio controller

--- src/core/breatheAudioController.ts
+++ src/core/breatheAudioController.ts
@@ -24,36 +24,39 @@ class BreatheAudioController {
     this.exhaleSound = await this.loadAudio(this.exhaleUrl);
   }
 
-  private playSound(audioBuffer: AudioBuffer, durationInSeconds: number): void {
+  private playSound(audioBuffer: AudioBuffer, durationInSeconds: number, volume: number): void {
     const source = this.audioContext.createBufferSource();
     source.buffer = audioBuffer;
 
     const playbackRate = source.buffer.duration / durationInSeconds;
     source.playbackRate.value = playbackRate;
 
-    source.connect(this.audioContext.destination);
+    const gainNode = this.audioContext.createGain();
+    gainNode.gain.value = volume;
+
+    source.connect(gainNode).connect(this.audioContext.destination);
     source.start();
   }
 
-  public playInhale(inhaleDuration: number): void {
+  public playInhale(inhaleDuration: number, volume: number = 1): void {
     if (!this.inhaleSound) {
       throw new Error("Inhale sound is not loaded. Call initialize() first.");
     }
-    this.playSound(this.inhaleSound, inhaleDuration);
+    this.playSound(this.inhaleSound, inhaleDuration, volume);
   }
 
-  public playExhale(exhaleDuration: number): void {
+  public playExhale(exhaleDuration: number, volume: number = 1): void {
     if (!this.exhaleSound) {
       throw new Error("Exhale sound is not loaded. Call initialize() first.");
     }
-    this.playSound(this.exhaleSound, exhaleDuration);
+    this.playSound(this.exhaleSound, exhaleDuration, volume);
   }
 }
 
 // Example usage:
 // const breatheAudioController = new BreatheAudioController('path_to_inhale.mp3', 'path_to_exhale.mp3');
 // await breatheAudioController.initialize();
-// breatheAudioController.playInhale(5);
-// breatheAudioController.playExhale(3);
+// breatheAudioController.playInhale(5, 0.8);
+// breatheAudioController.playExhale(3, 0.6);
 
 export default BreatheAudioController;
