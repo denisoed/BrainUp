cbe469382591958fb00c04c7fb8b2229cba30f59
Denisoed <denisoed@gmail.com>
2025-01-16 21:25:56 +0600

    Updated color for tg header and footer

--- src/boots/telegram.ts
+++ src/boots/telegram.ts
@@ -1,19 +1,13 @@
-import useColor from '@/composables/useColor';
-import useAvatar from '@/composables/useAvatar';
-import useUserApi from '@/api/useUserApi';
-import { useUserStore } from '@/stores/user';
 import { ACCESS_TOKEN_LOCAL_STORAGE_KEY } from '@/config';
 
 export default async () => {
-  const { rgbToHex } = useColor();
-
   const WebApp = window?.Telegram?.WebApp;
 
   try {
     if (WebApp) {
       WebApp.expand()
-      WebApp.setBackgroundColor(rgbToHex('rgb(240, 128, 128)'))
-      WebApp.setHeaderColor(rgbToHex('rgb(240, 128, 128)'))
+      WebApp.setBackgroundColor('#ffffff')
+      WebApp.setHeaderColor('#ffffff')
       WebApp.ready()
     }
   } catch {
