5fb4be52d300896ae903620b2df3bfe4ca835f61
Denisoed <denisoed@gmail.com>
2025-02-13 23:25:13 +0600

    Updated styles for home page

---
+++ src/components/CardComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <router-link :to="to || '/'" class="card">
+    <div class="card-title">{{ title }}</div>
+    <img v-if="icon" :src="icon" alt="icon" class="card-icon">
+  </router-link>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+  to: String,
+  icon: String,
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-height: 100px;
+  border-radius: 12px;
+  background-color: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  padding: 16px;
+  color: #fff;
+  position: relative;
+  overflow: hidden;
+
+  .card-icon {
+    width: 120px;
+    height: 120px;
+    position: absolute;
+    bottom: -40px;
+    right: -20px;
+    opacity: 0.1;
+  }
+}
+</style>
---
+++ src/components/Tabs/TabComp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab" :class="{ 'tab--active': active }">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  active: Boolean,
+});
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  padding: 6px 12px;
+  border-radius: 24px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 0.05);
+  line-height: normal;
+  text-align: center;
+
+  &.tab--active {
+    background-color: #fff;
+    color: #212121;
+  }
+}
+</style>
