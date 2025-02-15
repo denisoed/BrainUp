137adbc4b57cd7927e6c5cb891c119daba4a3ebe
Denisoed <denisoed@gmail.com>
2025-02-16 00:12:39 +0600

    Updated about dialog

--- src/components/Dialogs/AboutGameDialog.vue
+++ src/components/Dialogs/AboutGameDialog.vue
@@ -3,13 +3,10 @@
 		<div class="about-game-dialog flex column gap-sm">
 			<div class="close-line" />
 			<div class="about-game-dialog_content flex column items-center">
-				<div class="about-game-dialog_content-title mb-sm" v-html="$t(`games.${game}.about.title`)" />
-				<div class="about-game-dialog_content-descr mb-md" v-html="$t(`games.${game}.about.descr`)" />
-				<div class="about-game-dialog_content-rules mb-sm">
-					<span>{{ $t(`games.${game}.about.rules`) }}</span>
-				</div>
+				<div class="about-game-dialog_content-title mb-sm" v-html="$t(`games.howToPlay`)" />
 				<div class="about-game-dialog_content-rules mb-sm" v-html="$t(`games.${game}.about.rule1`)" />
-				<div class="about-game-dialog_content-rules mb-sm" v-html="$t(`games.${game}.about.rule2`)" />
+				<div class="about-game-dialog_content-rules mb-md" v-html="$t(`games.${game}.about.rule2`)" />
+				<div class="about-game-dialog_content-descr" v-html="$t(`games.${game}.about.descr`)" />
 				<Button
 					class="mt-md"
 					@click="onClose"
@@ -66,10 +63,7 @@ function onClose() {
 		:deep(.about-game-dialog_content-title) {
 			width: 100%;
 			font-size: 24px;
-
-			span {
-				color: var(--primary);
-			}
+			color: var(--primary);
 		}
 
 		:deep(.about-game-dialog_content-descr) {
@@ -89,7 +83,6 @@ function onClose() {
 			width: 100%;
 			font-size: 14px;
 			color: var(--white-color);
-			padding-left: 16px;
 			
 			span {
 				color: var(--primary);
