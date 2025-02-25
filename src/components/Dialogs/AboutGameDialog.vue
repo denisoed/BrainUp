dd63a5adf9dd3a13e50c2d9b582273580a536c73
Denisoed <denisoed@gmail.com>
2025-02-25 14:15:38 +0600

    Fix AboutGameDialog rendering and correct Breath game rule translation

--- src/components/Dialogs/AboutGameDialog.vue
+++ src/components/Dialogs/AboutGameDialog.vue
@@ -5,7 +5,10 @@
 			<div class="about-game-dialog_content flex column items-center">
 				<div class="about-game-dialog_content-title mb-sm" v-html="$t(`games.howToPlay`)" />
 				<div class="about-game-dialog_content-rules mb-sm" v-html="$t(`games.${game}.about.rule1`)" />
-				<div class="about-game-dialog_content-rules mb-md" v-html="$t(`games.${game}.about.rule2`)" />
+				<div
+					v-if="$t(`games.${game}.about.rule2`) !== `games.${game}.about.rule2`"
+					class="about-game-dialog_content-rules mb-md"
+					v-html="$t(`games.${game}.about.rule2`)" />
 				<div
 					v-if="$t(`games.${game}.about.rule3`) !== `games.${game}.about.rule3`"
 					class="about-game-dialog_content-rules mb-sm" 
