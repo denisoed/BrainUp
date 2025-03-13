949cd797a65100d76273ea27c7d89c1eeed3b76f
Denisoed <denisoed@gmail.com>
2025-03-13 21:00:19 +0600

    Добавить обработчик для сброса прокрутки страницы при изменении маршрута, улучшив пользовательский опыт навигации.

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,22 @@
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted, onUnmounted, watch } from 'vue';
 import AppContainerLayout from '@/layouts/index.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
 
 function vibrate() {
   if (window?.Telegram?.WebApp) {
     window.Telegram.WebApp.HapticFeedback.selectionChanged();
   }
 }
+
+watch(() => route.path, () => {
+  const app = document.getElementById('app');
+  app.scrollTo(0, 0);
+})
 
 onMounted(() => {
   document.addEventListener('click', vibrate);
