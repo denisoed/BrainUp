da17150a027d85e3a17aff76083f641b780f7556
Denisoed <denisoed@gmail.com>
2025-03-18 09:09:48 +0600

    Add JenesiusModal component to index layout; remove it from MainLayout for improved structure and configure dialog animation.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,18 +4,9 @@
       <slot />
     </div>
     <TheFooter />
-    <JenesiusModal />
   </div>
 </template>
 
 <script setup>
 import TheFooter from '@/components/TheFooter.vue';
-import {
-  container as JenesiusModal,
-  config as configDialog,
-} from 'jenesius-vue-modal';
-
-configDialog({
-  animation: "slide-bottom"
-})
 </script>
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -2,4 +2,16 @@
   <component :is="this.$route.meta.layoutComponent">
     <slot />
   </component>
+  <JenesiusModal />
 </template>
+
+<script setup>
+import {
+  container as JenesiusModal,
+  config as configDialog,
+} from 'jenesius-vue-modal';
+
+configDialog({
+  animation: "slide-bottom"
+})
+</script>
